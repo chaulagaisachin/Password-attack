0ba62c378d461823a6554bf753b352da528f3960
Sachin Chaulagai <50958807+chaulags@users.noreply.github.com>
2021-02-24 22:23:30 +0545

    encryption-technique

--- sha2-password.py
+++ sha2-password.py
@@ -1,1 +1,10 @@
 # This code help you to compare hashes and crack password using Dictionary
+import hashlib
+
+def encrypt_string(plain_string):
+    sha_signature = hashlib.sha256(plain_string.encode()).hexdigest()
+    return sha_signature
+
+plainText = str(input("Enter any string:- "))
+
+print(encrypt_string(plainText))
